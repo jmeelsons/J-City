ecf79df88573e9cf9ba3a157dcfe0421c8c6c18c
Jmeelson <sementulukov@gmail.com>
2026-07-12 11:31:19 +0300

    Create sh_vehicle_packs.lua

---
+++ lua/homigrad/sh_vehicle_packs.lua
@@ -0,0 +1,149 @@
+local modelAdjust = {
+	["models/left4dead/vehicles/apc_body_glide.mdl"] = {Vector(10.5,14,-1),Angle(0,-90,0),Vector(-10.5,14,-1),Angle(-180,90,0)},
+	["models/left4dead/vehicles/nuke_car_glide.mdl"] = {Vector(7,12,-1),Angle(0,-90,0),Vector(-7,12,-1),Angle(180,90,0)},
+	["models/gta5/vehicles/sanchez/chassis.mdl"] = {
+		Vector(15,17,-4.5),
+		Angle(-95,90,-90),
+		Vector(-15,17,-4.5),
+		Angle(-95,90,-90)},
+	["models/gta5/vehicles/wolfsbane/chassis.mdl"] = {
+		Vector(14.5,15.5,-7.5),
+		Angle(-95,90,-90),
+		Vector(-14.5,15.5,-7.5),
+		Angle(-95,90,-90)
+	},
+	["models/gta5/vehicles/blazer/chassis.mdl"] = {
+		Vector(13,11,-5),
+		Angle(-95,90,-90),
+		Vector(-13,11,-5),
+		Angle(-95,90,-90)
+	},
+	["models/gta5/vehicles/speedo/chassis.mdl"] = {
+		Vector(8,4,0),
+		Angle(0,-90,0),
+		Vector(-8,4,0),
+		Angle(0,-90,180)
+	},
+	["models/gta5/vehicles/dukes/chassis.mdl"] = {
+		Vector(7,6,0),
+		Angle(0,-80,0),
+		Vector(-7,6,0),
+		Angle(0,-100,180)
+	},
+	["models/gta5/vehicles/police/chassis.mdl"] = {
+		Vector(7.5,5,0),
+		Angle(0,-80,0),
+		Vector(-7.5,5,0),
+		Angle(0,-100,180)
+	},
+	["models/gta5/vehicles/hauler/chassis.mdl"] = {
+		Vector(10,4,0),
+		Angle(0,-90,0),
+		Vector(-10,4,0),
+		Angle(0,-90,180)
+	},
+	["models/blackterios_glide_vehicles/chevroletcorsaclassic/chevroletcorsaclassic.mdl"] = {
+		Vector(-9.5,3,0),
+		Angle(180,90,0),
+		Vector(9.5,3,0),
+		Angle(0,-90,0)
+	},
+	["models/blackterios_glide_vehicles/datsun510/datsun510.mdl"] = {
+		Vector(8.5,7.5,-1),
+		Angle(0,-90,0),
+		Vector(-8.5,7.5,-1),
+		Angle(0,-90,180)
+	},
+	["models/blackterios_glide_vehicles/fiatduna/fiatduna.mdl"] = {
+		Vector(8.5,3.5,-1),
+		Angle(0,-90,0),
+		Vector(-8.5,3.5,-1),
+		Angle(0,-90,180)
+	},
+	["models/blackterios_glide_vehicles/renaulttrafict1000d/renaulttrafict1000d.mdl"] = {
+		Vector(-11.5,7,0),
+		Angle(180,90,0),
+		Vector(11.5,7,0),
+		Angle(0,-90,0)
+	},
+	["models/blackterios_glide_vehicles/zanellarx150/zanellarx150.mdl"] = {
+		Vector(12,9.5,-9),
+		Angle(-70,0,0),
+		Vector(-12,9.5,-9),
+		Angle(-110,0,0)
+	},
+	["models/gta5/vehicles/seashark/chassis.mdl"] = {
+		Vector(11,-2,-16),
+		Angle(-35,80,-90),
+		Vector(-11,-2,-16),
+		Angle(-35,100,-90)
+	},
+	["models/hl2vehicles/muscle.mdl"] = {
+		Vector(9,3.9,0),
+		Angle(0,-90,5),
+		Vector(-9,3.9,0),
+		Angle(180,90,-5)
+		},
+		["models/blackterios_glide_vehicles/peugeot504injection/peugeot504injection.mdl"] = {
+			Vector(8.5,3.5,-1),
+			Angle(0,-90,0),
+			Vector(-8.5,3.5,-1),
+			Angle(0,-90,180)
+		},
+		["models/blackterios_glide_vehicles/2000fordf350superduty/2000fordf350superduty.mdl"] = {
+			Vector(8.5,3.5,-1),
+			Angle(0,-90,0),
+			Vector(-8.5,3.5,-1),
+			Angle(0,-90,180)
+		},
+		["models/blackterios_glide_vehicles/fordmercuryeightcoupe/fordmercuryeightcoupe.mdl"] = {
+			Vector(11.5,4,0),
+			Angle(180,90,180),
+			Vector(-11.5,4,0),
+			Angle(0,-90,180)
+		},
+		["models/blackterios_glide_vehicles/falconfutura67/falconfutura67.mdl"] = {
+			Vector(-10.5,4,0),
+			Angle(180,90,0),
+			Vector(10.5,4,0),
+			Angle(0,-90,0)
+		},
+		["models/blackterios_glide_vehicles/lambogallardolp570sl/lambogallardolp570sl.mdl"] = {
+			Vector(9,4,0),
+			Angle(180,90,180),
+			Vector(-9,4,0),
+			Angle(0,-90,180)
+		},
+		["models/blackterios_glide_vehicles/vwboravr6/vwboravr6.mdl"] = {
+			Vector(9,4,0),
+			Angle(180,90,180),
+			Vector(-9,4,0),
+			Angle(0,-90,180)
+		},
+		["models/blackterios_glide_vehicles/golg2plus/golg2plus.mdl"] = {
+			Vector(-9,4,0),
+			Angle(180,90,0),
+			Vector(9,4,0),
+			Angle(0,-90,0)
+		},
+		["models/blackterios_glide_vehicles/1998peugeot406/1998peugeot406.mdl"] = {
+			Vector(8,4,0),
+			Angle(180,90,180),
+			Vector(-8,4,0),
+			Angle(0,-90,180)
+		},				
+		["models/gta5/vehicles/seashark/chassis.mdl"] = {
+			Vector(11,-2,-16),
+			Angle(-35,80,-90),
+			Vector(-11,-2,-16),
+			Angle(-35,100,-90)
+		},
+		["models/hl2vehicles/muscle.mdl"] = {
+			Vector(9,3.9,0),
+			Angle(0,-90,5),
+			Vector(-9,3.9,0),
+			Angle(180,90,-5)        
+    },
+}
+
+return modelAdjust
